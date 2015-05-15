8e89a8087450ce1f4fcf09ee0570b3a55f22b7bb
Georg Kohlweiss <kohlweiss@gmail.com>
2015-05-15 23:10:16 +0200

    fix wrong comments

--- lib/yagen-api/src/main/java/com/github/gekoh/yagen/util/HSQLFixLobDialect.java
+++ lib/yagen-api/src/main/java/com/github/gekoh/yagen/util/HSQLFixLobDialect.java
@@ -1,15 +1,24 @@
 /*
- * HSQLDialect
- * Copyright (c) 2012 CREDIT SUISSE Technology and Operations. All Rights Reserved.
- * This software is the proprietary information of CREDIT SUISSE Technology and Operations.
- * Use is subject to license and non-disclosure terms.
- */
+ Copyright 2014 Georg Kohlweiss
+
+ Licensed under the Apache License, Version 2.0 (the License);
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an AS IS BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 package com.github.gekoh.yagen.util;
 
 import java.sql.Types;
 
 /**
- * @author Georg Kohlweiss (F477448)
+ * @author Georg Kohlweiss
  */
 public class HSQLFixLobDialect extends org.hibernate.dialect.HSQLDialect {
     //private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(HSQLDialect.class);
