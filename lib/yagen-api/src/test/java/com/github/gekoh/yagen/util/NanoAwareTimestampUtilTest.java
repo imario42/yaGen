0068ffc680146f2d2845cf73e0d1a54b4c17d3d7
Georg Kohlweiss <kohlweiss@gmail.com>
2020-05-06 09:50:08 +0200

    fix timing issue in test, seems like eg. in VM we don't get another nano timestamp when called in direct sequence

--- lib/yagen-api/src/test/java/com/github/gekoh/yagen/util/NanoAwareTimestampUtilTest.java
+++ lib/yagen-api/src/test/java/com/github/gekoh/yagen/util/NanoAwareTimestampUtilTest.java
@@ -13,7 +13,12 @@ public class NanoAwareTimestampUtilTest {
     public void testConsecutiveInvocations() {
         for (int i = 0; i < 1000; i++) {
             Timestamp timestamp1 = NanoAwareTimestampUtil.getCurrentTimestamp();
+            String timestamp1Str = timestamp1.toString();
             Timestamp timestamp2 = NanoAwareTimestampUtil.getCurrentTimestamp();
+            String timestamp2Str = timestamp2.toString();
+            if (!timestamp2.after(timestamp1)) {
+                System.err.println(timestamp2Str + " is not after " + timestamp1Str);
+            }
             assertTrue(timestamp2.after(timestamp1));
         }
     }
